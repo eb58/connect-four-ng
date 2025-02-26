0c94415c3135435e6e793e02ff8a50cc2b6ce46f
Erich Brandl <erich.brandl@gmail.com>
2025-02-26 15:31:57 +0100

    negascout.js

--- src/app/services/negascout.js
+++ src/app/services/negascout.js
@@ -1,15 +1,15 @@
-let negascout = (state, depth, maxDepth, alpha, beta) => {
-  if (state.isMill) return -MAXVAL + depth
-  if (MOVES.every(m => state.heightCols[m] >= DIM.NROW)) return 0
-  if (depth === maxDepth) return computeScoreOfNode(state);
-  let [lo, hi] = [alpha, beta];
-  for (const m of MOVES) if (state.heightCols[m] < DIM.NROW) {
-    let t = -negascout(doMove(m, cloneState(state)), depth + 1, maxDepth, -hi, -lo)
-    if (lo < t && t < beta && j > 0 && depth < maxDepth - 1)
-      t = -negascout(doMove(m, cloneState(state)), depth + 1, maxDepth, -beta, -t)
-    if (t > lo) lo = t
-    if (lo >= beta) return lo
-    hi = lo + 1
-  }
-  return lo;
-}
+// let negascout = (state, depth, maxDepth, alpha, beta) => {
+//   if (state.isMill) return -MAXVAL + depth
+//   if (MOVES.every(m => state.heightCols[m] >= DIM.NROW)) return 0
+//   if (depth === maxDepth) return computeScoreOfNode(state);
+//   let lo = alpha, hi = beta;
+//   for (let j = 0; j < 7; j++) if (state.heightCols[MOVES[j]] < DIM.NROW) {
+//     let t = -negascout(doMove(MOVES[j], cloneState(state)), depth + 1, maxDepth, -hi, -lo)
+//     if (lo < t && t < beta && j > 0 && depth < maxDepth - 1)
+//       t = -negascout(doMove(MOVES[j], cloneState(state)), depth + 1, maxDepth, -beta, -t)
+//     if (t > lo) lo = t
+//     if (lo >= beta) return lo
+//     hi = lo + 1
+//   }
+//   return lo;
+// }
